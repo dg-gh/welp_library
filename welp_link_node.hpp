c1779e787b394aa95058e2163154ffb25b6ce347
Dave <52504186+dg-gh@users.noreply.github.com>
2020-12-15 18:12:49 +0100

    Update welp_link_node.hpp

--- welp_link_node.hpp
+++ welp_link_node.hpp
@@ -1,4 +1,4 @@
-// welp_link_node.hpp - last update : 12 / 12 / 2020
+// welp_link_node.hpp - last update : 15 / 12 / 2020
 // License <http://unlicense.org/> (statement below at the end of the file)
 
 
@@ -11,6 +11,10 @@
 #include <cstdlib>
 #include <map>
 
+#ifdef WELP_LINK_NODE_INCLUDE_MUTEX
+#include <mutex>
+#endif // WELP_LIN_NODE_INCLUDE_MUTEX
+
 
 ////// DESCRIPTIONS //////
 
@@ -20,10 +24,6 @@ namespace welp
 	// link_node can add both an observer_node and a link_node with the method add_observer_node
 	template<class msg_Ty, class label_Ty = int, class _Allocator = std::allocator<std::pair<char* const, label_Ty>>> class link_node
 	{
-
-	private:
-
-		std::map<char*, label_Ty, std::less<char*>, _Allocator> node_map;
 
 	public:
 
@@ -59,7 +59,59 @@ namespace welp
 		link_node(welp::link_node<msg_Ty, label_Ty, _Allocator>&&) = default;
 		welp::link_node<msg_Ty, label_Ty, _Allocator>& operator=(welp::link_node<msg_Ty, label_Ty, _Allocator>&&) = default;
 		virtual ~link_node() = default;
+
+	private:
+
+		std::map<char*, label_Ty, std::less<char*>, _Allocator> node_map;
 	};
+
+#ifdef WELP_LINK_NODE_INCLUDE_MUTEX
+	// the allocator must have std::pair<char* const, label_Ty>> as template parameter
+	// link_node_sync can add both an observer_node and a link_node_sync with the method add_observer_node
+	template<class msg_Ty, class label_Ty = int, class _Allocator = std::allocator<std::pair<char* const, label_Ty>>> class link_node_sync
+	{
+
+	public:
+
+		// getting notifications
+
+		virtual void on_notification_from_node(const msg_Ty& msg) {}
+
+
+		// sending notifications
+
+		void notify_all_observer_nodes(const msg_Ty& msg);
+		void notify_all_observer_nodes(const msg_Ty& msg, const label_Ty& compare_label);
+		template <class Predicate> void notify_all_observer_nodes(const msg_Ty& msg, Predicate Pr);
+
+
+		// observer nodes management
+
+		template <class Ty> void add_observer_node(Ty* target_ptr);
+		template <class Ty> void add_observer_node(Ty* target_ptr, const label_Ty& target_label);
+		template <class Ty> void remove_observer_node(Ty* target_ptr);
+		inline void remove_all_observer_nodes() noexcept;
+		void remove_all_observer_nodes(const label_Ty& target_label);
+		template <class Predicate> void remove_all_observer_nodes(Predicate Pr);
+		template <class Ty> bool contains_observer_node(Ty* target_ptr) const;
+		inline std::size_t observer_node_count() const noexcept;
+		std::size_t observer_node_count(const label_Ty& target_label) const;
+		template <class Predicate> std::size_t observer_node_count(Predicate Pr) const;
+
+
+		link_node_sync() = default;
+		link_node_sync(const welp::link_node_sync<msg_Ty, label_Ty, _Allocator>&) = default;
+		welp::link_node_sync<msg_Ty, label_Ty, _Allocator>& operator=(const welp::link_node_sync<msg_Ty, label_Ty, _Allocator>&) = default;
+		link_node_sync(welp::link_node_sync<msg_Ty, label_Ty, _Allocator>&&) = default;
+		welp::link_node_sync<msg_Ty, label_Ty, _Allocator>& operator=(welp::link_node_sync<msg_Ty, label_Ty, _Allocator>&&) = default;
+		virtual ~link_node_sync() = default;
+
+	private:
+
+		std::map<char*, label_Ty, std::less<char*>, _Allocator> node_map;
+		std::mutex link_node_mutex;
+	};
+#endif // WELP_LIN_NODE_INCLUDE_MUTEX
 
 	template <class msg_Ty> class observer_node
 	{
@@ -118,7 +170,6 @@ void welp::link_node<msg_Ty, label_Ty, _Allocator>::notify_all_observer_nodes(co
 		}
 	}
 }
-
 
 template <class msg_Ty, class label_Ty, class _Allocator> template <class Ty>
 void welp::link_node<msg_Ty, label_Ty, _Allocator>::add_observer_node(Ty* target_ptr)
@@ -226,6 +277,166 @@ std::size_t welp::link_node<msg_Ty, label_Ty, _Allocator>::observer_node_count(P
 	}
 	return counter;
 }
+
+
+#ifdef WELP_LINK_NODE_INCLUDE_MUTEX
+template <class msg_Ty, class label_Ty, class _Allocator>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::notify_all_observer_nodes(const msg_Ty& msg)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	for (auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+		iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end(); ++iter)
+	{
+		iter->first->on_notification_from_node(msg);
+	}
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::notify_all_observer_nodes(const msg_Ty& msg, const label_Ty& compare_label)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	for (auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+		iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end(); ++iter)
+	{
+		if (iter->second == compare_label)
+		{
+			iter->first->on_notification_from_node(msg);
+		}
+	}
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Predicate>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::notify_all_observer_nodes(const msg_Ty& msg, Predicate Pr)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	for (auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+		iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end(); ++iter)
+	{
+		if (Pr(iter->second))
+		{
+			iter->first->on_notification_from_node(msg);
+		}
+	}
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Ty>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::add_observer_node(Ty* target_ptr)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	node_map.emplace(static_cast<char*>(static_cast<void*>(target_ptr)), label_Ty());
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Ty>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::add_observer_node(Ty* target_ptr, const label_Ty& target_label)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	node_map.emplace(static_cast<char*>(static_cast<void*>(target_ptr)), target_label);
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Ty>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::remove_observer_node(Ty* target_ptr)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	node_map.erase(static_cast<char*>(static_cast<void*>(target_ptr)));
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator>
+inline void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::remove_all_observer_nodes() noexcept
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	node_map.clear();
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::remove_all_observer_nodes(const label_Ty& target_label)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+	while (iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end())
+	{
+		if (iter->second == target_label)
+		{
+			iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	}
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Predicate>
+void welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::remove_all_observer_nodes(Predicate Pr)
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+	while (iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end())
+	{
+		if (Pr(iter->second))
+		{
+			iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	}
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Ty>
+bool welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::contains_observer_node(Ty* target_ptr) const
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	if (node_map.count(static_cast<char*>(static_cast<void*>(target_ptr))) == 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator>
+inline std::size_t welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::observer_node_count() const noexcept
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	return node_map.size();
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator>
+std::size_t welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::observer_node_count(const label_Ty& compare_label) const
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	std::size_t counter = 0;
+	for (auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+		iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end(); ++iter)
+	{
+		if (iter->second == compare_label)
+		{
+			counter++;
+		}
+	}
+	return counter;
+}
+
+template <class msg_Ty, class label_Ty, class _Allocator> template <class Predicate>
+std::size_t welp::link_node_sync<msg_Ty, label_Ty, _Allocator>::observer_node_count(Predicate Pr) const
+{
+	std::lock_guard<std::mutex> lock(link_node_mutex);
+	std::size_t counter = 0;
+	for (auto iter = reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).begin();
+		iter != reinterpret_cast<std::map<welp::link_node_sync<msg_Ty, label_Ty, _Allocator>*, label_Ty>&>(node_map).end(); ++iter)
+	{
+		if (Pr(iter->second))
+		{
+			counter++;
+		}
+	}
+	return counter;
+}
+#endif // WELP_LIN_NODE_INCLUDE_MUTEX
+
 
 #endif // WELP_LINK_NODE_H
 
