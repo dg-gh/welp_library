a338da71765923c73a23cc2003d6d282bbac0673
Dave <52504186+dg-gh@users.noreply.github.com>
2020-12-27 01:35:14 +0100

    Update welp_threads.hpp

--- welp_threads.hpp
+++ welp_threads.hpp
@@ -25,7 +25,11 @@
 #endif // WELP_THREADS_INCLUDE_ALL
 
 
-#if defined(WELP_THREADS_DEBUG_MODE) || defined(WELP_ALWAYS_DEBUG_MODE)
+#if defined(WELP_ALWAYS_DEBUG_MODE) && !defined(WELP_THREADS_DEBUG_MODE)
+#define WELP_THREADS_DEBUG_MODE
+#endif // WELP_ALWAYS_DEBUG_MODE
+
+#ifdef WELP_THREADS_DEBUG_MODE
 #include <iostream>
 #ifdef WELP_THREADS_INCLUDE_FSTREAM
 #include <fstream>
