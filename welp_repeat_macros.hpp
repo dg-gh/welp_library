a69c4e75ccfcf1bd1f51fc4652a1666158f98ead
Dave <52504186+dg-gh@users.noreply.github.com>
2021-01-31 20:37:07 +0100

    Create welp_repeat_macros.hpp

---
+++ welp_repeat_macros.hpp
@@ -0,0 +1,104 @@
+// welp_repeat_macros.hpp - last update : 31 / 01 / 2021
+// License <http://unlicense.org/> (statement below at the end of the file)
+
+
+#ifndef WELP_REPEAT_MACROS_HPP
+#define WELP_REPEAT_MACROS_HPP
+
+#define welp_repeat_x1(welp_statement) welp_statement;
+#define welp_repeat_x2(welp_statement) welp_statement; welp_statement;
+#define welp_repeat_x3(welp_statement) welp_repeat_x2(welp_statement) welp_statement;
+#define welp_repeat_x4(welp_statement) welp_repeat_x2(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x5(welp_statement) welp_repeat_x4(welp_statement) welp_statement;
+#define welp_repeat_x6(welp_statement) welp_repeat_x4(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x7(welp_statement) welp_repeat_x4(welp_statement) welp_repeat_x2(welp_statement) welp_statement;
+#define welp_repeat_x8(welp_statement) welp_repeat_x4(welp_statement) welp_repeat_x4(welp_statement)
+
+#define welp_repeat_x9(welp_statement) welp_repeat_x8(welp_statement) welp_statement;
+#define welp_repeat_x10(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x11(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x3(welp_statement)
+#define welp_repeat_x12(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x4(welp_statement)
+#define welp_repeat_x13(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x5(welp_statement)
+#define welp_repeat_x14(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x6(welp_statement) 
+#define welp_repeat_x15(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x7(welp_statement) 
+#define welp_repeat_x16(welp_statement) welp_repeat_x8(welp_statement) welp_repeat_x8(welp_statement)
+
+#define welp_repeat_x17(welp_statement) welp_repeat_x16(welp_statement) welp_statement;
+#define welp_repeat_x18(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x19(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x3(welp_statement)
+#define welp_repeat_x20(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x4(welp_statement)
+#define welp_repeat_x21(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x5(welp_statement)
+#define welp_repeat_x22(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x6(welp_statement) 
+#define welp_repeat_x23(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x7(welp_statement) 
+#define welp_repeat_x24(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x8(welp_statement)
+
+#define welp_repeat_x25(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x9(welp_statement)
+#define welp_repeat_x26(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x10(welp_statement)
+#define welp_repeat_x27(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x11(welp_statement)
+#define welp_repeat_x28(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x12(welp_statement)
+#define welp_repeat_x29(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x13(welp_statement)
+#define welp_repeat_x30(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x14(welp_statement) 
+#define welp_repeat_x31(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x15(welp_statement) 
+#define welp_repeat_x32(welp_statement) welp_repeat_x16(welp_statement) welp_repeat_x16(welp_statement)
+
+
+#define welp_repeat_x33(welp_statement) welp_repeat_x32(welp_statement) welp_statement;
+#define welp_repeat_x34(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x35(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x3(welp_statement)
+#define welp_repeat_x36(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x4(welp_statement)
+#define welp_repeat_x37(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x5(welp_statement)
+#define welp_repeat_x38(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x6(welp_statement) 
+#define welp_repeat_x39(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x7(welp_statement) 
+#define welp_repeat_x40(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x8(welp_statement)
+
+#define welp_repeat_x41(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x9(welp_statement)
+#define welp_repeat_x42(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x10(welp_statement)
+#define welp_repeat_x43(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x11(welp_statement)
+#define welp_repeat_x44(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x12(welp_statement)
+#define welp_repeat_x45(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x13(welp_statement)
+#define welp_repeat_x46(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x14(welp_statement) 
+#define welp_repeat_x47(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x15(welp_statement) 
+#define welp_repeat_x48(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x16(welp_statement)
+
+#define welp_repeat_x49(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x17(welp_statement)
+#define welp_repeat_x50(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x18(welp_statement)
+#define welp_repeat_x51(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x19(welp_statement)
+#define welp_repeat_x52(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x20(welp_statement)
+#define welp_repeat_x53(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x21(welp_statement)
+#define welp_repeat_x54(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x22(welp_statement) 
+#define welp_repeat_x55(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x23(welp_statement) 
+#define welp_repeat_x56(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x24(welp_statement)
+
+#define welp_repeat_x57(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x25(welp_statement)
+#define welp_repeat_x58(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x26(welp_statement)
+#define welp_repeat_x59(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x27(welp_statement)
+#define welp_repeat_x60(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x28(welp_statement)
+#define welp_repeat_x61(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x29(welp_statement)
+#define welp_repeat_x62(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x30(welp_statement) 
+#define welp_repeat_x63(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x31(welp_statement) 
+#define welp_repeat_x64(welp_statement) welp_repeat_x32(welp_statement) welp_repeat_x32(welp_statement)
+
+
+#define welp_repeat_x01(welp_statement) welp_statement;
+#define welp_repeat_x02(welp_statement) welp_statement; welp_statement;
+#define welp_repeat_x03(welp_statement) welp_repeat_x2(welp_statement) welp_statement;
+#define welp_repeat_x04(welp_statement) welp_repeat_x2(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x05(welp_statement) welp_repeat_x4(welp_statement) welp_statement;
+#define welp_repeat_x06(welp_statement) welp_repeat_x4(welp_statement) welp_repeat_x2(welp_statement)
+#define welp_repeat_x07(welp_statement) welp_repeat_x4(welp_statement) welp_repeat_x2(welp_statement) welp_statement;
+#define welp_repeat_x08(welp_statement) welp_repeat_x4(welp_statement) welp_repeat_x4(welp_statement)
+#define welp_repeat_x09(welp_statement) welp_repeat_x8(welp_statement) welp_statement;
+
+#endif // WELP_REPEAT_MACROS_HPP
+
+
+// welp_repeat_macros.hpp
+// 
+// This is free software released into the public domain.
+// 
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software for any purpose and by any means.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY IN CONNECTION WITH THE SOFTWARE.
